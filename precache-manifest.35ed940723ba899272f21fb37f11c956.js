3086d41f636cac2b128d73fe3eb7260efe5ead4c
herman-woo <hermanwoo100@gmail.com>
2023-09-20 22:34:38 -0400

    Updates

---
+++ precache-manifest.35ed940723ba899272f21fb37f11c956.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8c0dd8d954da1423f85",
+    "url": "/spacestagram/static/css/main.4ba06045.chunk.css"
+  },
+  {
+    "revision": "a8c0dd8d954da1423f85",
+    "url": "/spacestagram/static/js/main.a8c0dd8d.chunk.js"
+  },
+  {
+    "revision": "d2507d2ae27f9c7aa57a",
+    "url": "/spacestagram/static/js/1.d2507d2a.chunk.js"
+  },
+  {
+    "revision": "993f6ee410c3651a7733",
+    "url": "/spacestagram/static/js/runtime~main.993f6ee4.js"
+  },
+  {
+    "revision": "93ce8cc8e6a49c80e2771f6d386c60d9",
+    "url": "/spacestagram/static/media/sync.93ce8cc8.svg"
+  },
+  {
+    "revision": "ad04c91fdb92d3e17947064b3ecb8811",
+    "url": "/spacestagram/static/media/default.ad04c91f.jpg"
+  },
+  {
+    "revision": "e4fb69f53ad401c0ccba99f7a7f43048",
+    "url": "/spacestagram/static/media/unlike.e4fb69f5.svg"
+  },
+  {
+    "revision": "276455fb1e8ed828b30285357fc6f31c",
+    "url": "/spacestagram/static/media/home.276455fb.svg"
+  },
+  {
+    "revision": "4006e702fe6586abba7e60ce6f962333",
+    "url": "/spacestagram/static/media/search.4006e702.svg"
+  },
+  {
+    "revision": "ccf77d455972429416835e1a4dbbea2f",
+    "url": "/spacestagram/static/media/liked.ccf77d45.svg"
+  },
+  {
+    "revision": "ee40c7546a5b4e3dc7cc23c5c5d23302",
+    "url": "/spacestagram/index.html"
+  }
+];
